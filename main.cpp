5a917607309669f300aee83b303ee531da68880d
luminglin <lu.minglin@foxmail.com>
2018-01-25 22:18:33 +0800

    add {opengl glut opencv} libs

--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
-#include <stdio.h>
-//#include <utils/io_utils.h>
-#include <opencv2/opencv.hpp>
- 
-using namespace cv;
- 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
- 
-    Mat image;
-    image = imread( argv[1], 1 );
- 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
- 
-    waitKey(0);
- 
+//  main.cpp
+//  OpenGL_01_HelloWorld
+//
+//  Created by apple on 14/12/28.
+//  Copyright (c) 2014年 cc. All rights reserved.
+//
+
+#include <iostream>
+#include <GLUT/GLUT.h>
+
+void draw() {
+    
+    //设置清屏色
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    //设置颜色，红色
+    glColor3f(1.0f, 0.0f, 0.0f);
+    //设置绘图时的坐标系统
+    glOrtho(0.0f, 1.0f, 0.0f, 1.0f, -1.0f, 1.0f);
+    //开始渲染
+    glBegin(GL_POLYGON);
+    //设置多边形的4个顶点
+    glVertex3f(0.25f, 0.25f, 0.0f);
+    glVertex3f(0.75f, 0.25f, 0.0f);
+    glVertex3f(0.75f, 0.75f, 0.0f);
+    glVertex3f(0.25f, 0.75f, 0.0f);
+    //结束渲染
+    glEnd();
+    //强制刷新缓冲区，保证绘制命令被执行
+    glFlush();
+    
+}
+
+int main(int argc, const char* argv[]) {
+    //初始化GLUT库
+    glutInit(&argc, (char**)argv);
+    //创建一个窗口并制定窗口名
+    glutCreateWindow("HelloWorld");
+    //注册一个绘图函数，操作系统在必要时刻就会对窗体进行重新绘制操作
+    glutDisplayFunc(draw);
+    //进入GLUT事件处理循环，让所有的与“事件”有关的函数调用无限循环(永生循环)
+    glutMainLoop();
     return 0;
 }
+
+
+
